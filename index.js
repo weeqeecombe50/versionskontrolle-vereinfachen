bf932b5b91162b9235a1564bc4279f069ae424bd
weeqeecombe50 <mynieagbona@hotmail.com>
2025-10-01 18:16:24 +0300

    Updated welcome message for clarity.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the simplified version control tutorial!');
+  res.send('Welcome to the version control tutorial. Enjoy exploring!');
 });
 
 app.listen(PORT, () => {
